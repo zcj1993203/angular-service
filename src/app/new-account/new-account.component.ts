f1ca79b796c3c4384088e5bdbf8d00f1bf473182
John <zcj1993lif@gmail.com>
2019-05-22 15:49:36 -0400

    use hierachy injector to make there is the same instance of service in all components

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -5,7 +5,7 @@ import { AccountsService } from '../accounts.service';
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService, AccountsService]//let angular know how to provide the service
+  providers: [LoggingService]//let angular know how to provide the service
 })
 export class NewAccountComponent {
   
