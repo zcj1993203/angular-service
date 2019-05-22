a981727f393004504a1e431b33f61b244441d26f
John <zcj1993lif@gmail.com>
2019-05-22 14:40:35 -0400

    create logging service

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-
+import { LoggingService } from '../logging.service';
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
@@ -13,6 +13,8 @@ export class NewAccountComponent {
       name: accountName,
       status: accountStatus
     });
-    console.log('A server status changed, new status: ' + accountStatus);
+    const service = new LoggingService();
+    service.logStatusChange(accountStatus);
+    //console.log('A server status changed, new status: ' + accountStatus);
   }
 }
